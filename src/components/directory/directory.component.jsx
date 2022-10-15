771419a519599377e4600ed1d507b7b8ac3c98a1
Hazem Abdulmoneim <hazem.mando408@gmail.com>
2022-10-15 09:22:25 +0200

    a bit of refactoring

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,37 +2,9 @@ import "./directory.styles.scss";
 import CategoryItem from "../category-item/category-item.component";
 import React from "react";
 
-const Directory = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
+const Directory = ({ categories }) => {
   return (
-    <div className="categories-container">
+    <div className="directory-container">
       {categories.map((category) => {
         return <CategoryItem category={category} key={category.id} />;
       })}
